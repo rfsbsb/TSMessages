82290ec35e494305cc8b19eafc91edb3c6d9b02d
Rafael Silva <invites@rafaelsilva.net>
2015-11-15 10:29:45 -0200

    Adding basic data for framework

---
+++ TSMessages/TSMessages.h
@@ -0,0 +1,19 @@
+//
+//  TSMessages.h
+//  TSMessages
+//
+//  Created by Rafael Ferreira Silva on 14/11/15.
+//  Copyright © 2015 Rafael Silva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TSMessages.
+FOUNDATION_EXPORT double TSMessagesVersionNumber;
+
+//! Project version string for TSMessages.
+FOUNDATION_EXPORT const unsigned char TSMessagesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TSMessages/PublicHeader.h>
+
+
